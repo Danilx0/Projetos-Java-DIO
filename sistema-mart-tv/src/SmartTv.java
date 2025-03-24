82e8eb7fa13cb3961d482762ba7e5ff5e0bc0ff3
Bowie <marketing@letscode.com.br>
2025-03-23 23:35:31 -0300

    Seguimente da aula - Classes e Metodos

---
+++ sistema-mart-tv/src/SmartTv.java
@@ -0,0 +1,14 @@
+public class SmartTv {
+    boolean ligada=false;
+    int canal=10;
+    int volume=14;
+
+    public void ligar() {
+      ligada = true;
+    }
+
+    public void desliga(){
+      ligada = false;
+    }
+    
+}
---
+++ sistema-mart-tv/src/Usuario.java
@@ -0,0 +1,11 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("A tv está ? " +smartTv.ligada);
+        System.out.println("A tv está no canal: " +smartTv.canal);
+        System.out.println("Volume atual: " +smartTv.volume);
+
+    }
+}
