21c33a029786c6ea549502827c7167ffed188e63
Bowie <marketing@letscode.com.br>
2025-03-29 12:04:03 -0300

    Criação e configuração do arquivo .json

--- Terminal-Java/src/App.java
+++ Terminal-Java/src/App.java
@@ -1,5 +1,13 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+
+        String nome = args[0];
+        String sobrenome = args[1];
+        int idade = Integer.valueOf(args[2]);
+        Double altura = Double.valueOf(args[3]);
+
+        System.out.println("Seu nome é: " + nome + " " +sobrenome);
+        System.out.println("Sua idade é: " + idade + " anos");
+        System.out.println("Você tem: " + altura + " de altura");
     }
 }
