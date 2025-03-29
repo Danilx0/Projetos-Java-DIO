3ebc06f8b4dc8d3db1e9cf83447ab0e1be84c3dc
Bowie <marketing@letscode.com.br>
2025-03-29 11:30:04 -0300

    Criação de arquivos e execuções de ações no terminal

---
+++ Terminal-Java/src/ClasseTerminal.java
@@ -0,0 +1,8 @@
+public class ClasseTerminal {
+  public static void main(String[] args) {
+    System.out.println("Executando pelo terminal");
+  
+    System.out.println("Teste atualização terminal");
+  }
+
+}
